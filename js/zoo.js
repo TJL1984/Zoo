1826a1b0b3d8a683f7b20f41bd3193176d756f82
TJ Liwanag <timothyjliwanag@gmail.com>
2016-11-15 14:17:46 -0500

    Created 2 animals. I need properties for both.

--- js/zoo.js
+++ js/zoo.js
@@ -1,7 +1,20 @@
 (function() {
   'use strict';
 
-function Animal
+  function Animal(name) {
+    console.log( '', this.name);
+    this.name = name;
+    console.log(this.name);
+  }
+  Animal.prototype.growl = function growl() {
+    console.log
+  }
+
+  var tony = new Animal('Tony')
+    console.log(a)
+
+  var harambe = new Animal('Harambe')
+    console.log(b)
 
 
 
@@ -9,4 +22,9 @@ function Animal
 
 
 
-}());
+
+  function Tiger (name, birthdate) {
+
+  }
+
+})();
