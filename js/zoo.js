af58dbdc9052800c5d3488a79d3ec9cedaeeb9c4
TJ Liwanag <timothyjliwanag@gmail.com>
2016-11-15 13:51:30 -0500

    initial commit

---
+++ js/zoo.js
@@ -0,0 +1,22 @@
+(function() {
+  'use strict';
+
+  function Animal(species) {
+    console.log( 'first animal', this );
+    this.animal = name || 'Miscellaneous';
+  }
+  Animal.prototype.growl = function growl() {
+    console.log( 'Growl!' + this.animal);
+  };
+
+
+
+function Animal
+
+
+
+
+
+
+
+}());
