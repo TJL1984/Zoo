97663b2442c670ee97df02c66362522d9a0fd823
TJ Liwanag <timothyjliwanag@gmail.com>
2016-11-16 18:54:20 -0500

    I was able to run 2 tests for each animal

--- src/js/zoo.js
+++ src/js/zoo.js
@@ -1,6 +1,11 @@
 (function() {
   'use strict';
 
+  window.zoo = window.zoo || {};
+  window.zoo.Animal = Animal;
+  window.zoo.Tiger = Tiger;
+  window.zoo.Gorilla = Gorilla;
+
   /**
    * Produces name and age when creating a new animal.
    * @param {String} name identifies the animal's name
@@ -8,22 +13,27 @@
    */
 
   function Animal(name, birthDate) {
-    console.log(this.name);
+    // console.log(this.name);
     this.name = name || 'Miscellaneous';
-    console.log(this.name);
+    // console.log(this.name);
     this.birthDate = birthDate || 'Name Unknown';
   }
 
   Animal.prototype.growl = function growl() {
-    console.log
-  }
+    console.log('growl');
+  };
 
-  var Tiger = new Animal('Tony', 2007)
-    console.log(Tiger)
+  // var Tiger = new Animal('Tony', 2007)
+  //   console.log(Tiger)
 
-  var Gorilla = new Animal('Harambe', 2001)
-    console.log(Gorilla)
+  // var Gorilla = new Animal('Harambe', 2001)
+  //   console.log(Gorilla)
 
+  /**
+   * Produces name and age when creating a Tiger.
+   * @param {String} name identifies the tiger's name
+   * @param {Number} birthDate identifies the tiger's age in years
+   */
   function Tiger(name, birthDate) {
     Animal.apply(this, [name, birthDate]);
   }
@@ -31,38 +41,42 @@
   // Need to create tiger prototype
 Tiger.prototype = Object.create(Animal.prototype);
 Tiger.prototype.constructor = Tiger;
-Tiger.prototype.sprint = function sprint(duration) {
-  return ("laps");
-}
+Tiger.prototype.kill = function kill() {
+  return 'chew' + kill + 'prey';
+};
+
 
 Tiger.prototype.newBorn = function newBorn() {
-  var tigerBaby = new Tiger('baby tiger', 2016);
-  return newBorn;
-}
+  return new Tiger('baby tiger', 2015);
+};
 
-var babyTiger = new Animal ('Tigger', 2015)
+var babyTiger = new Tiger ('Tigger', 1);
   console.log(babyTiger);
   console.log(babyTiger.newBorn() );
-  console.log(babyTiger.spring() )
+  console.log(babyTiger.kill() );
 
+  /**
+   * Produces name and age when creating a new gorilla.
+   * @param {String} name identifies the gorilla's name
+   * @param {Number} birthDate identifies the gorilla's age in years
+   */
   function Gorilla(name, birthDate) {
     Animal.apply(this, [name, birthDate] );
   }
 
-  // Need to create gorilla prototype
 Gorilla.prototype = Object.create(Animal.prototype);
 Gorilla.prototype.constructor = Gorilla;
-Gorilla.prototype.climb = function climb(duration) {
-  return("treeclimber");
+Gorilla.prototype.slam = function slam() {
+  return("Fist bump");
+};
 
 Gorilla.prototype.newBorn = function newBorn() {
-  var gorillaBaby = new Gorilla('baby gorilla', 2015);
-  return newBorn;
-}
+  return new Gorilla('baby gorilla', 2014);
+};
 
-var babyGorilla = new Animal ('Kerchak', 2011);
+var babyGorilla = new Gorilla ('Kerchak', 2);
   console.log(babyGorilla);
   console.log(babyGorilla.newBorn() );
-  console.log(babyGorilla.climb("All the way up") );
+  console.log(babyGorilla.slam("Fist bump") );
 
 })();
--- test/spec/zoo-test.spec.js
+++ test/spec/zoo-test.spec.js
@@ -1,29 +1,55 @@
 (function() {
   'use strict';
-
   window.zoo = window.zoo || {};
-
   var expect = chai.expect;
 
-    describe('testing the namespace', function() {
+    describe('Testing the namespace', function() {
       console.log('I am here');
-    });
 
-        it('Should confirm namespace is window.zoo', function (){
+        it('Should confirm namespace is window.zoo', function() {
         expect(window.zoo.Animal).to.be.a('function');
+        });
       });
 
 
-    // describe(Testing animal constructor function, function (){
-    //     it(Should produce new animal) (){
-    //     expect(window.zoo.Animal)to.be.an('object');
-    //     });
-    //     it(Should return undefined if an array is in the input) (){
-    //     expect(window.zoo.Animal)to
-    //     });
-    //   });
+    describe('Testing animal constructor function', function() {
+      console.log('test for Animals');
 
+        it('should produce new Animal', function() {
+          expect(new window.zoo.Animal('Tony', 9)).to.be.an('object');
+        });
 
+        it('should make a prototype animal when given no arguments', function() {
+          expect(new window.zoo.Animal({})).to.be.an('object');
+        });
+    });
+
+    describe('Tiger constructor function', function() {
+      console.log('test for Tiger');
+
+        it('should make a Tiger when called', function() {
+          var Tigger = new window.zoo.Tiger('Tigger', 1);
+          expect(Tigger).to.be.an.instanceof(window.zoo.Tiger);
+        });
+        it('The birthDate argument should be a number', function () {
+          var celebrant = new window.zoo.Tiger('celebrant', 2015);
+          expect(celebrant.birthDate).to.be.a('Number');
+        });
+
+    });
+
+    describe('Gorilla constructor function', function() {
+      console.log('test for Gorilla');
+        it('should make a Gorilla when called', function() {
+          var Kerchak = new window.zoo.Gorilla('Kerchak', 2);
+          expect(Kerchak).to.be.an.instanceof(window.zoo.Gorilla);
+        });
+        it('The birthDate argument should be a number', function () {
+          var celebrant = new window.zoo.Gorilla('celebrant', 2015);
+          expect(celebrant.birthDate).to.be.a('Number');
+        });
+
+    });
 
 
 }());
