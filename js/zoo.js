1fb770daa7a6c87ed9aa2a40ca9fecd35292fb08
TJ Liwanag <timothyjliwanag@gmail.com>
2016-11-15 23:28:06 -0500

    created prototypes for each animal, need to test

--- js/zoo.js
+++ js/zoo.js
@@ -11,7 +11,7 @@
     console.log(this.name);
     this.name = name || 'Miscellaneous';
     console.log(this.name);
-    this.birthDate = birthDate || 'Not Known';
+    this.birthDate = birthDate || 'Name Unknown';
   }
 
   Animal.prototype.growl = function growl() {
@@ -28,7 +28,41 @@
     Animal.apply(this, [name, birthDate]);
   }
 
+Tiger.prototype = Object.create(Animal.prototype);
+Tiger.prototype.constructor = Tiger;
 
+Tiger.prototype.sprint = function sprint(duration) {
+  return ("laps");
+}
 
+Tiger.prototype.newBorn = function newBorn() {
+  var tigerBaby = new Tiger('baby tiger', 2016);
+  return baby;
+}
+
+var babyTiger = new Animal ('Tigger', 2015)
+  console.log(babyTiger);
+  console.log(babyTiger.newBorn() );
+  console.log(babyTiger.spring() )
+
+  function Gorilla(name, birthDate) {
+    Animal.apply(this, [name, birthDate]);
+  }
+
+Gorilla.prototype = Object.create(Animal.prototype);
+Gorilla.prototype.constructor = Gorilla;
+
+Gorilla.prototype.climb = function climb(duration) {
+  return("treeclimber");
+
+Gorilla.prototype.newBorn = function newBorn() {
+  var gorillaBaby = new Gorilla('baby gorilla', 2015);
+  return baby;
+}
+
+var babyGorilla = new Animal ('Kerchak', 2011);
+  console.log(babyGorilla);
+  console.log(babyGorilla.newBorn() );
+  console.log(babyGorilla.climb("All the way up") );
 
 })();
